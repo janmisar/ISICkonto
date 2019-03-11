b508074acc836278e4e2d545c6f352e484b2c23b
Rostislav Babáček <babacros@fit.cvut.cz>
2019-03-11 17:49:48 +0100

    Create project

---
+++ ISICbalance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ISICbalance
+//
+//  Created by Rostislav Babáček on 10/03/2019.
+//  Copyright © 2019 Rostislav Babáček. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
