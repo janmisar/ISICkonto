abaa0aabefe410cc1b04b09d4446fdc8d5daa1f9
Vendula Svastalova <svastven@fit.cvut.cz>
2019-03-14 23:47:41 +0100

    create result wrapper

---
+++ ISICkonto/Support/Result.swift
@@ -0,0 +1,30 @@
+//
+//  Result.swift
+//  ISICkonto
+//
+//  Created by Vendula Švastalová on 13/03/2019.
+//  Copyright © 2019 Vendula Švastalová. All rights reserved.
+//
+
+import Foundation
+
+public enum ResultInformation {
+    case inProgress, success, failure, loggedIn
+}
+
+public struct Result<Element> {
+    public let info: ResultInformation
+    public let message: String?
+    public let element: Element?
+    
+    public init(info: ResultInformation, message: String?, element: Element?) {
+        self.info = info
+        self.message = message
+        self.element = element
+    }
+    
+    static public func resultFailed(_ message: String? = nil) -> Result { return Result(info: .failure, message: message, element: nil) }
+    static public func resultSuccess(_ message: String? = nil, element: Element) -> Result { return Result(info: .success, message: message, element: element) }
+    static public func resultInProgress() -> Result { return Result(info: .inProgress, message: nil, element: nil) }
+    static public func resultLoggedIn(_ message: String? = nil, element: Element) -> Result { return Result(info: .loggedIn, message: nil, element: element) }
+}
