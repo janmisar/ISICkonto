9ae5392b338537f8e7a713ba2888e128c954c00f
Rostislav Babáček <babacros@fit.cvut.cz>
2019-03-15 11:34:31 +0100

    Extension group. Base viewModel/viewController file was added.

---
+++ ISICbalance/Extensions/ColorExt.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExt.swift
+//  ISICbalance
+//
+//  Created by Rostislav Babáček on 11/03/2019.
+//  Copyright © 2019 Rostislav Babáček. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    struct MyTheme {
+        static let labelBlue = UIColor(red: 171/255, green: 213/255, blue: 242/255, alpha: 1)
+        static let backgroundColor = UIColor(red: 75/255, green: 150/255, blue: 179/255, alpha: 1)
+        static let textFieldBackground = UIColor(red: 55/255, green: 131/255, blue: 160/255, alpha: 1)
+        static let textColor = UIColor.white
+    }
+}
---
+++ ISICbalance/Extensions/TextFieldExt.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldExt.swift
+//  ISICbalance
+//
+//  Created by Rostislav Babáček on 15/03/2019.
+//  Copyright © 2019 Rostislav Babáček. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
